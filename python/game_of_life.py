a9e9174dee7e291924ea480f5d75a520057cb6ea
tkrajina <tkrajina@gmail.com>
2011-12-03 11:18:11 +0100

    Initial upload

---
+++ python/game_of_life.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+
+import random as mod_random
+import os as mod_os
+
+DEFAULT_SIZE = 50
+
+def new_array():
+	table = []
+
+	for i in range( DEFAULT_SIZE ):
+		row = []
+		for j in range( DEFAULT_SIZE ):
+			row.append( 0 )
+		table.append( row )
+
+	return table
+
+def iteration( table ):
+	""" Returns a new array based on "Game of life" """
+
+	result_table = new_array()
+
+	for i in range( DEFAULT_SIZE ):
+		for j in range( DEFAULT_SIZE ):
+			cell = table[ i ][ j ]
+
+			live_neighbours_no = 0
+
+			live_neighbours_no += table[ ( i + 1 ) % DEFAULT_SIZE ][ ( j - 1 ) % DEFAULT_SIZE ]
+			live_neighbours_no += table[ ( i + 1 ) % DEFAULT_SIZE ][ j ]
+			live_neighbours_no += table[ ( i + 1 ) % DEFAULT_SIZE ][ ( j + 1 ) % DEFAULT_SIZE ]
+			live_neighbours_no += table[ i ][ ( j - 1 ) % DEFAULT_SIZE ]
+			live_neighbours_no += table[ i ][ ( j + 1 ) % DEFAULT_SIZE ]
+			live_neighbours_no += table[ ( i - 1 ) % DEFAULT_SIZE ][ ( j - 1 ) % DEFAULT_SIZE ]
+			live_neighbours_no += table[ ( i - 1 ) % DEFAULT_SIZE ][ j ]
+			live_neighbours_no += table[ ( i - 1 ) % DEFAULT_SIZE ][ ( j + 1 ) % DEFAULT_SIZE ]
+
+			# 1. Any live cell with fewer than two live neighbours dies, as if caused by under-population:
+			if cell and live_neighbours_no < 2:
+				result_table[ i ][ j ] = 0
+
+			# 2. Any live cell with two or three live neighbours lives on to the next generation.
+			elif cell and live_neighbours_no == 2 or live_neighbours_no == 3:
+				result_table[ i ][ j ] = 1
+
+			# 3. Any live cell with more than three live neighbours dies, as if by overcrowding.
+			elif cell and live_neighbours_no > 3:
+				result_table[ i ][ j ] = 0
+
+			# 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+			elif not cell and live_neighbours_no == 3:
+				result_table[ i ][ j ] = 1
+
+			else:
+				result_table[ i ][ j ] = 0
+
+	return result_table
+
+def clear_screen():
+	mod_os.system( 'clear' )
+
+def print_table( table ):
+	result = ''
+	for i in range( DEFAULT_SIZE ):
+		for j in range( DEFAULT_SIZE ):
+			cell = table[ i ][ j ]
+			if cell:
+				result += '*'
+			else:
+				result += ' '
+		result += '|\n'
+	print result
+
+table = new_array()
+
+# Fill with random values:
+for i in range( DEFAULT_SIZE ):
+	for j in range( DEFAULT_SIZE ):
+		table[ i ][ j ] = 1 if mod_random.randint( 0, 3 ) == 0 else 0
+
+for i in range( 1000000 ):
+	clear_screen()
+
+	print_table( table )
+
+	table = iteration( table )
+
+	raw_input( 'Next?' )
