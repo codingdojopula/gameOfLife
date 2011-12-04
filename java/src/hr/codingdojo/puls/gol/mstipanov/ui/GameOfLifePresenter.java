5004594e6677933eba0cb5aea12c7cf5f0b39349
Marko Stipanov <mstipanov@ib-mstipanov.(none)>
2011-12-04 21:08:18 +0100

    java code init.ver.

---
+++ java/src/hr/codingdojo/puls/gol/mstipanov/ui/GameOfLifePresenter.java
@@ -0,0 +1,17 @@
+package hr.codingdojo.puls.gol.mstipanov.ui;
+
+import hr.codingdojo.puls.gol.mstipanov.Cell;
+
+/**
+ * @author mstipanov
+ * @since 04.12.11. 18:57
+ */
+public interface GameOfLifePresenter {
+    void draw(Cell[][] cells);
+
+    void live(int x, int y);
+
+    void die(int x, int y);
+
+    void show();
+}
